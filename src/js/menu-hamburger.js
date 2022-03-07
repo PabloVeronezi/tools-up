79c09eb62a83c6068782ff11c4c9cfd5d2f535f9
Pablo Veronezi <pabloorempuller@hotmail.com>
2022-03-07 19:24:07 -0300

    New update

--- src/js/menu-hamburger.js
+++ src/js/menu-hamburger.js
@@ -1,14 +1,17 @@
 const menu = document.querySelector(".navbar-menu");
 const btn_burger = document.querySelector(".navbar-hamburger");
 const closeMenu_btn = document.querySelector(".close-menu");
+const btn_cart = document.querySelector(".navbar-cart");
 const login_area = document.querySelector(".navbar-login");
 
 btn_burger.addEventListener("click", _ => {
   menu.classList.toggle("active");
   login_area.classList.toggle("active-login");
+  btn_cart.classList.toggle("active-cart");
 });
 
 closeMenu_btn.addEventListener("click", _ => {
   menu.classList.toggle("active");
   login_area.classList.toggle("active-login");
+  btn_cart.classList.toggle("active-cart");
 })
